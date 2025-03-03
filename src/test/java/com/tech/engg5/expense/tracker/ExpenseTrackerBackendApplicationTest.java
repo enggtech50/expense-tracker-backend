b931756ccec56e5f584dec8aa648cbd0fd2db31e
tech-engg5 <enggtech50@gmail.com>
2025-03-03 10:45:25 +0530

    Initial skeleton structure

---
+++ src/test/java/com/tech/engg5/expense/tracker/ExpenseTrackerBackendApplicationTest.java
@@ -0,0 +1,9 @@
+package com.tech.engg5.expense.tracker;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class ExpenseTrackerBackendApplicationTest {
+}
